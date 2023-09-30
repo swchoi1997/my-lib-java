757de1dfb6de7bc5b0c16ca6704e4d7d1e78aa28
상원 최 <swchoi1@swm.ai>
2023-09-30 16:00:23 +0900

    add: #9-database-connector delegate andthen compose 추가

--- src/main/java/org/example/delegate/Func1.java
+++ src/main/java/org/example/delegate/Func1.java
@@ -1,5 +1,18 @@
 package org.example.delegate;
 
-public interface Func1<T1, R> extends Delegate{
+import java.util.Objects;
+
+@FunctionalInterface
+public interface Func1<T1, R> extends Delegate {
     R invork(T1 t1) throws Exception;
+
+    default <V> Func1<V, R> compose(Func1<? super V, ? extends T1> before) {
+        Objects.requireNonNull(before);
+        return (V v) -> invork(before.invork(v));
+    }
+
+    default <V> Func1<T1, V> andThen(Func1<? super R, ? extends V> after) {
+        Objects.requireNonNull(after);
+        return (T1 t1) -> after.invork(invork(t1));
+    }
 }
