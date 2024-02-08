ace751a3ca0ea1b58b8204c66cff58fe0a4672ca
SangWon <swchoi1997@naver.com>
2024-02-08 22:22:32 +0900

    add: WaitingThreadPool 수정

--- src/main/java/org/example/thread/policy/WaitingThreadExceptionPolicy.java
+++ src/main/java/org/example/thread/policy/WaitingThreadExceptionPolicy.java
@@ -1,21 +1,30 @@
 package org.example.thread.policy;
 
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class WaitingThreadExceptionPolicy implements RejectedExecutionHandler {
 
     private final BlockingQueue<Runnable> blockingQueue;
+    private final AtomicBoolean mainQueueFullFlag;
 
-    public WaitingThreadExceptionPolicy(){
-        this(new LinkedBlockingQueue<>());
+    public WaitingThreadExceptionPolicy(final AtomicBoolean mainQueueFullFlag){
+        this(new LinkedBlockingQueue<>(), mainQueueFullFlag);
     }
 
-    public WaitingThreadExceptionPolicy(BlockingQueue<Runnable> blockingQueue) {
+    public WaitingThreadExceptionPolicy(BlockingQueue<Runnable> blockingQueue, final AtomicBoolean mainQueueFullFlag) {
         this.blockingQueue = blockingQueue;
+        this.mainQueueFullFlag = mainQueueFullFlag;
+
     }
 
     @Override
     public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+        System.out.println("test123123123");
+        if (!this.mainQueueFullFlag.get()) {
+            this.mainQueueFullFlag.set(true);
+        }
+
         boolean added = this.blockingQueue.offer(r);
         if (!added) {
             throw new RejectedExecutionException();
@@ -32,5 +41,9 @@ public class WaitingThreadExceptionPolicy implements RejectedExecutionHandler {
 
     public Integer getWaitingQueueCount() {
         return this.blockingQueue.size();
+    }
+
+    public AtomicBoolean getMainQueueFullFlag() {
+        return mainQueueFullFlag;
     }
 }
--- src/main/java/org/example/thread/pool/DynamicThreadPool.java
+++ src/main/java/org/example/thread/pool/DynamicThreadPool.java
@@ -3,8 +3,11 @@ package org.example.thread.pool;
 import org.example.thread.policy.WaitingThreadExceptionPolicy;
 
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-public class DynamicThreadPool extends ThreadPoolExecutor {
+public final class DynamicThreadPool extends ThreadPoolExecutor {
+
+    private final AtomicBoolean mainQueueFullFlag;
 
     public static ThreadPoolExecutor newDynamicThreadPool(int corePoolSize, int maximumPoolSize) {
         return new DynamicThreadPool(corePoolSize, maximumPoolSize);
@@ -20,7 +23,10 @@ public class DynamicThreadPool extends ThreadPoolExecutor {
                 60L,
                 TimeUnit.SECONDS,
                 new ArrayBlockingQueue<>(maximumPoolSize),
-                new WaitingThreadExceptionPolicy());
+                new WaitingThreadExceptionPolicy(new AtomicBoolean(false)));
+
+        final WaitingThreadExceptionPolicy handler = (WaitingThreadExceptionPolicy) this.getRejectedExecutionHandler();
+        this.mainQueueFullFlag = handler.getMainQueueFullFlag();
     }
 
     private DynamicThreadPool(int corePoolSize, int maximumPoolSize, BlockingQueue<Runnable> queue) {
@@ -29,19 +35,41 @@ public class DynamicThreadPool extends ThreadPoolExecutor {
                 60L,
                 TimeUnit.SECONDS,
                 queue,
-                new WaitingThreadExceptionPolicy());
+                new WaitingThreadExceptionPolicy(new AtomicBoolean(false)));
+
+        final WaitingThreadExceptionPolicy handler = (WaitingThreadExceptionPolicy) this.getRejectedExecutionHandler();
+        this.mainQueueFullFlag = handler.getMainQueueFullFlag();
+    }
+
+    @Override
+    public void execute(Runnable command) {
+        if (this.mainQueueFullFlag.get()) {
+            final WaitingThreadExceptionPolicy handler =
+                    (WaitingThreadExceptionPolicy) this.getRejectedExecutionHandler();
+
+            handler.getBlockingQueue().offer(command);
+            return;
+        }
+
+        super.execute(command);
     }
 
     @Override
     protected void beforeExecute(Thread t, Runnable r) {
-        final WaitingThreadExceptionPolicy waitingHandler =
+        final WaitingThreadExceptionPolicy handler =
                 (WaitingThreadExceptionPolicy) this.getRejectedExecutionHandler();
+
         synchronized (this.getQueue()) {
-            final Runnable poll = waitingHandler.poll();
+            final Runnable poll = handler.poll();
             if (poll != null) {
                 this.getQueue().offer(poll);
             }
+
+            if (this.getQueue().size() < this.getMaximumPoolSize() && handler.getBlockingQueue().isEmpty()) {
+                this.mainQueueFullFlag.compareAndSet(true, false);
+            }
         }
+        super.beforeExecute(t, r);
     }
 
     public Integer getWaitingQueueTaskCount() {
@@ -49,5 +77,47 @@ public class DynamicThreadPool extends ThreadPoolExecutor {
                 (WaitingThreadExceptionPolicy) this.getRejectedExecutionHandler();
 
         return waitingHandler.getWaitingQueueCount();
+    }
+
+
+    public static void main(String[] args) {
+        ThreadPoolExecutor executor =
+                DynamicThreadPool.newDynamicThreadPool(2, 10);
+
+        for (int i = 0; i < 100; i++) {
+            final int jobCnt = i;
+            executor.submit(() -> {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println(Thread.currentThread().getName() + " ->>>>>> Running" + jobCnt);
+            });
+        }
+        int cnt = 0;
+        while (true) {
+            WaitingThreadExceptionPolicy rejectedExecutionHandler = (WaitingThreadExceptionPolicy) executor.getRejectedExecutionHandler();
+            System.out.println(rejectedExecutionHandler.getBlockingQueue().size());
+            try {
+                Thread.sleep(200);
+                cnt += 1;
+                if (cnt % 10 == 0){
+                    for (int i = 0; i < 20; i++) {
+                        final int jobCnt = i;
+                        executor.submit(() -> {
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                            System.out.println(Thread.currentThread().getName() + " ->>>>>> Running" + jobCnt);
+                        });
+                    }
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
